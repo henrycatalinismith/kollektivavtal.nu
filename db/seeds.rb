30aa5b391df2fa432bbb8d07ea4f8bacc35e971e
Henry Catalini Smith <henry@catalinismith.se>
2024-10-10 20:36:49 +0200

    rails 8 working

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,16 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+Blog::Post.create({
+  "id" => "01j9pj4rhj26hn83pcb5fwb3y3",
+  "title"=>"First Contact",
+  "body"=>"Imagine a website that's advocating for organising in Sweden regardless of union affiliation or politics. An informative hub for what a CBA is and what unions do in Sweden. Maybe even a petition tool to help build leverage for new CBAs to be signed ([see pitch](/pitch/)).",
+  "published_at"=> "2024-09-17",
+  "image"=>"/pitch/pitch.png",
+  "slug"=>"first-contact",
+})
+Blog::Post.create({
+  "id" => "01j9pj52k685xf601c75zt4cms",
+  "title"=>"Pre-Flight Checks",
+  "body"=>"We're measuring demand. Do you feel like there's an information gap regarding collective bargaining in Sweden? A lack of linkable multilingual resources answering common questions? Would your campaign benefit from a privacy-first petition tool that's not limited by the jurisdictional boundaries between the various unions at your workplace?\n\nThen enter your email address to get updates on the project. The more email addresses, the stronger the signal that we're not alone in wanting this. Share it with friends and colleagues who you think will be interested too.",
+  "published_at"=> "2024-10-31",
+  "image"=>"/artemis.jpg",
+  "slug"=>"pre-flight-checks",
+})
