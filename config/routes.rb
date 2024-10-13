ff0bd6aed1ecf63098b82ed828c46a0e1f0c3f18
Henry Catalini Smith <henry@catalinismith.se>
2024-10-13 08:13:53 +0200

    admin

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   root "home#index"
   post "contacts" => "contacts#create"
   get "up" => "rails/health#show", as: :rails_health_check
