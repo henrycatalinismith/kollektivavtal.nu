7bade2c399a241ec4666d5adeb5252a4946c9acf
Henry Catalini Smith <henry@catalinismith.com>
2024-10-13 16:10:17 +0200

    add status

--- app/models/mailing_list/subscription.rb
+++ app/models/mailing_list/subscription.rb
@@ -1,2 +1,7 @@
 class MailingList::Subscription < ApplicationRecord
+  enum :sendgrid_status, {
+    sendgrid_pending: 0,
+    sendgrid_success: 1,
+    sendgrid_failure: 2,
+  }
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2024_10_13_130414) do
+ActiveRecord::Schema[8.0].define(version: 2024_10_13_135757) do
   create_table "blog_posts", id: :string, default: -> { "ULID()" }, force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
@@ -43,6 +43,7 @@ ActiveRecord::Schema[8.0].define(version: 2024_10_13_130414) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "email"
+    t.integer "sendgrid_status"
   end
 
   create_table "user_accounts", id: :string, default: -> { "ULID()" }, force: :cascade do |t|
