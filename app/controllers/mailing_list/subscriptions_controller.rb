bf5ceb6f81463701cd273a7a71a18448560d1967
Henry Catalini Smith <henry@catalinismith.se>
2024-10-13 16:15:24 +0200

    make sendgrid api call toggleable

--- app/controllers/mailing_list/subscriptions_controller.rb
+++ app/controllers/mailing_list/subscriptions_controller.rb
@@ -1,28 +1,31 @@
 class MailingList::SubscriptionsController < ApplicationController
   def create
     sendgrid_status = :sendgrid_pending
-    begin
-      sg = SendGrid::API.new(api_key: ENV['SENDGRID_API_KEY'])
-      response = sg.client.marketing.contacts.put(
-        request_body: {
-          list_ids: [ENV["SENDGRID_LIST_ID"]],
-          contacts: [{
-            email: params[:email]
-          }]
-        }
-      )
-    rescue Exception => e
-      Rails.logger.error e.message
-      sendgrid_status = :sendgrid_failure
-    else
-      sendgrid_status = :sendgrid_success
-    ensure
-      MailingList::Subscription.create!(
-        email: params[:email],
-        sendgrid_status:,
-      )
-      flash[:notice] = "Signed up successfully!"
-      redirect_to root_path
+
+    if Flipper.enabled?(:mailing_list_subscriptions_create_sendgrid, current_user)
+      begin
+        sg = SendGrid::API.new(api_key: ENV['SENDGRID_API_KEY'])
+        response = sg.client.marketing.contacts.put(
+          request_body: {
+            list_ids: [ENV["SENDGRID_LIST_ID"]],
+            contacts: [{
+              email: params[:email]
+            }]
+          }
+        )
+      rescue Exception => e
+        Rails.logger.error e.message
+        sendgrid_status = :sendgrid_failure
+      else
+        sendgrid_status = :sendgrid_success
+      end
     end
+
+    MailingList::Subscription.create!(
+      email: params[:email],
+      sendgrid_status:,
+    )
+    flash[:notice] = "Signed up successfully!"
+    redirect_to root_path
   end
 end
