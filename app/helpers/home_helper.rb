c748b30ca2351619d43d9ceef382d77d7df03ccc
Henry Catalini Smith <henry@catalinismith.se>
2024-10-15 09:00:06 +0200

    translations

--- app/helpers/home_helper.rb
+++ app/helpers/home_helper.rb
@@ -1,2 +1,13 @@
 module HomeHelper
+  def render_home_index_description_markdown(markdown)
+    renderer = HomeDescriptionRender.new()
+    redcarpet = Redcarpet::Markdown.new(renderer, autolink: true, tables: true)
+    redcarpet.render(markdown)
+  end
+end
+
+class HomeDescriptionRender < Redcarpet::Render::HTML
+  def paragraph(text)
+    "<p class=\"mt-4 text-sm/6 text-gray-300\">#{text}</p>"
+  end
 end
