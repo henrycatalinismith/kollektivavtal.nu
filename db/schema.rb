f9b7c28724ac086458cf0fb992f6078e70807090
Henry Catalini Smith <henry@catalinismith.se>
2024-10-15 09:16:14 +0200

    translateable posts

--- db/schema.rb
+++ db/schema.rb
@@ -10,15 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2024_10_13_135757) do
+ActiveRecord::Schema[8.0].define(version: 2024_10_15_071030) do
   create_table "blog_posts", id: :string, default: -> { "ULID()" }, force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "title", null: false
-    t.text "body", null: false
+    t.string "title_en", null: false
+    t.text "body_en", null: false
     t.date "published_at"
     t.string "image"
     t.string "slug"
+    t.string "title_sv"
+    t.string "body_sv"
     t.index ["published_at"], name: "index_blog_posts_on_published_at"
     t.index ["slug"], name: "index_blog_posts_on_slug", unique: true
   end
