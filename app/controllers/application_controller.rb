024c0b33a982a8fa14048f9fa94de282c889d16f
Henry Catalini Smith <henry@catalinismith.se>
2024-10-15 09:08:19 +0200

    add swedish

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,10 @@
 class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
+
+  before_action :set_locale
+
+  def set_locale()
+    I18n.locale = http_accept_language.compatible_language_from(I18n.available_locales) || I18n.default_locale
+  end
 end
