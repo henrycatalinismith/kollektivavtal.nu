555e8a7702085146d28604274fa9572438cf880a
Henry Catalini Smith <henry@catalinismith.com>
2024-10-15 21:04:29 +0200

    admin

--- config/initializers/rails_admin.rb
+++ config/initializers/rails_admin.rb
@@ -30,13 +30,61 @@ RailsAdmin.config do |config|
   end
 
   config.model "User::Account" do
+    list do
+      configure :email do
+        sticky true
+        column_width 256
+      end
+    end
+
     object_label_method do
       :email
+    end
+  end
+
+  config.model "User::Authorization" do
+    list do
+      configure :account do
+        sticky true
+        column_width 256
+      end
+
+      configure :role do
+        sticky true
+        column_width 256
+      end
+    end
+  end
+
+  config.model "User::Role" do
+    list do
+      configure :name do
+        sticky true
+        column_width 256
+      end
     end
   end
 
   config.model "Blog::Post" do
     configure :body_en, :text
     configure :body_sv, :text
+
+    configure :title_en do
+      sticky true
+      column_width 256
+    end
+  end
+
+  config.model "MailingList::Subscription" do
+    configure :email do
+      sticky true
+      column_width 256
+    end
+    configure :sendgrid_status do
+      sticky true
+    end
+    configure :turnstile_status do
+      sticky true
+    end
   end
 end
