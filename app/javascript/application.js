7f4d08498acddb106727cad1afebf01afc66a893
Henry Catalini Smith <henry@catalinismith.com>
2024-10-18 09:39:15 +0200

    nav

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,2 +1,11 @@
-// Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 console.log("⭐️ kollektivavtal.nu")
+
+document.addEventListener('DOMContentLoaded', () => {
+  const burger = document.querySelector('[aria-controls="mobile-menu"]')
+  const menu = document.querySelector('#mobile-menu')
+
+  burger.addEventListener('click', () => {
+    menu.classList.toggle('hidden')
+    burger.setAttribute('aria-expanded', menu.classList.contains('hidden') ? 'false' : 'true')
+  })
+})
