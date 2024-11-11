ea780322b1823e6e610da0bdf6fe765473e13aca
Henry Catalini Smith <henry@catalinismith.se>
2024-11-11 20:56:32 +0100

    add socials

--- app/models/social_media/account.rb
+++ app/models/social_media/account.rb
@@ -4,5 +4,16 @@ class SocialMedia::Account < ApplicationRecord
   enum :provider, {
     facebook: 0,
     linkedin: 1,
+    x: 2,
   }
+
+  rails_admin do
+    list do
+      field :rank
+      field :name
+      field :url
+      field :created_at
+      sort_by :rank
+    end
+  end
 end
