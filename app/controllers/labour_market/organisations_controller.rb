14ddcfa8a079411121d165de39f523d0694291d6
Henry Catalini Smith <henry@catalinismith.com>
2024-11-13 13:36:26 +0100

    a-z

--- app/controllers/labour_market/organisations_controller.rb
+++ app/controllers/labour_market/organisations_controller.rb
@@ -6,6 +6,7 @@ class LabourMarket::OrganisationsController < ApplicationController
     @organisations = LabourMarket::Organisation
       .where(organisation_type: params[:organisation_type])
       .order(I18n.locale == :sv ? :name_sv : :name_en)
+    @letters = @organisations.map { |o| o.name[0].upcase }.uniq.sort
   end
 
   def show
