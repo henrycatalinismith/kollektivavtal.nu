554d466ea4fae2f4d6148432ab942e311f6f87c5
Henry Catalini Smith <henry@catalinismith.se>
2024-11-14 21:02:39 +0100

    index

--- app/controllers/labour_market_controller.rb
+++ app/controllers/labour_market_controller.rb
@@ -1,9 +1,39 @@
+
 class LabourMarketController < ApplicationController
   layout "page"
 
   def index
-    @highlighted_collective_agreements = LabourMarket::Agreement.first(3)
-    @highlighted_local_unions = LabourMarket::Organisation.local_union.first(3)
-    @highlighted_central_unions = LabourMarket::Organisation.central_union.first(3)
+    collective_agreement_slugs = [
+      "tekniktjansteavtalet",
+      "motorbranschavtalet",
+      "techsverige",
+      "industriavtalet",
+    ]
+
+    @highlighted_collective_agreements = LabourMarket::Agreement
+      .where(slug: collective_agreement_slugs)
+      .sort_by { |a| collective_agreement_slugs.index(a.slug) }
+
+    local_union_slugs = [
+      "spotify-workers-unionen",
+      "seko-klubb-sj-trafik",
+      "volvo-car-verkstadsklubb",
+      "sveriges-ingenjorer-pa-sandvik",
+    ]
+
+    @highlighted_local_unions = LabourMarket::Organisation
+      .where(slug: local_union_slugs)
+      .sort_by { |a| local_union_slugs.index(a.slug) }
+
+    central_union_slugs = [
+      "if-metall",
+      "unionen",
+      "kommunal",
+      "sveriges-ingenjorer",
+    ]
+
+    @highlighted_central_unions = LabourMarket::Organisation
+      .where(slug: central_union_slugs)
+      .sort_by { |a| central_union_slugs.index(a.slug) }
   end
 end
