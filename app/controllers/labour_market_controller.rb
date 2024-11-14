b400ebecf7537926d1c8fd466763a9b0bdae9850
Henry Catalini Smith <henry@catalinismith.com>
2024-11-14 16:13:02 +0100

    index page

--- app/controllers/labour_market_controller.rb
+++ app/controllers/labour_market_controller.rb
@@ -2,5 +2,8 @@ class LabourMarketController < ApplicationController
   layout "page"
 
   def index
+    @highlighted_collective_agreements = LabourMarket::Agreement.first(3)
+    @highlighted_local_unions = LabourMarket::Organisation.local_union.first(3)
+    @highlighted_central_unions = LabourMarket::Organisation.central_union.first(3)
   end
 end
