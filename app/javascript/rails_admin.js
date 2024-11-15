e25d2fb020a783914eb6589545cd2178d875dcc7
Henry Catalini Smith <henry@catalinismith.se>
2024-11-15 22:02:55 +0100

    fix

--- app/javascript/rails_admin.js
+++ app/javascript/rails_admin.js
@@ -5,33 +5,41 @@ import List from '@editorjs/list';
 
 console.log("⭐️ kollektivavtal.nu admin")
 
-document.addEventListener("DOMContentLoaded", () => {
-  const editorContainer = document.querySelector(".editorjs-container");
-  const encodedValue = editorContainer.getAttribute('data-editorjs-value');
-  const decodedValue = atob(encodedValue); 
-  const editorData = JSON.parse(JSON.parse(decodedValue));
 
-  if (editorContainer) {
-    const editorInstance = new EditorJS({
-      holder: editorContainer.id,
-      tools: {
-        header: {
-          class: Header,
-          inlineToolbar: true,
+document.addEventListener("DOMContentLoaded", () => {
+  const editorContainers = document.querySelectorAll(".editorjs-container");
+  editorContainers.forEach((editorContainer) => {
+    const encodedValue = editorContainer.getAttribute('data-editorjs-value');
+    const decodedValue = atob(encodedValue); 
+    let editorData = {};
+    try {
+      editorData = JSON.parse(JSON.parse(decodedValue));
+    } catch (error) {
+      console.error('Error parsing editor data', error);
+    }
+
+    if (editorContainer) {
+      const editorInstance = new EditorJS({
+        holder: editorContainer.id,
+        tools: {
+          header: {
+            class: Header,
+            inlineToolbar: true,
+          },
+          list: {
+            class: List,
+            inlineToolbar: true,
+          },
         },
-        list: {
-          class: List,
-          inlineToolbar: true,
+        data: editorData,
+        onChange: async () => {
+          const savedData = await editorInstance.save();
+          const hiddenInput = document.querySelector(`input[name="${editorContainer.dataset.target}"]`);
+          if (hiddenInput) {
+            hiddenInput.value = JSON.stringify(savedData);
+          }
         },
-      },
-      data: editorData,
-      onChange: async () => {
-        const savedData = await editorInstance.save();
-        const hiddenInput = document.querySelector(`input[name="${editorContainer.dataset.target}"]`);
-        if (hiddenInput) {
-          hiddenInput.value = JSON.stringify(savedData);
-        }
-      },
-    });
-  }
+      });
+    }
+  })
 });
