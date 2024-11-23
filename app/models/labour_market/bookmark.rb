ee88b74f3474d9b1f380d34682304a707dbb2977
Henry Catalini Smith <henry@catalinismith.se>
2024-11-23 07:50:28 +0100

    source

--- app/models/labour_market/bookmark.rb
+++ app/models/labour_market/bookmark.rb
@@ -1,4 +1,10 @@
 class LabourMarket::Bookmark < ApplicationRecord
-  belongs_to :source, class_name: "LabourMarket::Document", foreign_key: "source_id"
+  belongs_to :source, class_name: "LabourMarket::Document", foreign_key: "source_id", optional: true
   validates :url, presence: true
+end
+
+documents = LabourMarket::Document.where.not(source_url: nil)
+documents.each do |document|
+  bookmark = LabourMarket::Bookmark.find_or_create_by(url: document.source_url)
+  document.update(source: bookmark)
 end
