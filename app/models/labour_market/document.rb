e0574aaa7bd74cc081bc66cf9955c3c6f314f0d0
Henry Catalini Smith <henry@catalinismith.se>
2024-11-25 21:34:18 +0100

    done for tonight
    horrible fucking fly.io incident broke the db. fixed tho

--- app/models/labour_market/document.rb
+++ app/models/labour_market/document.rb
@@ -1,13 +1,11 @@
 class LabourMarket::Document < ApplicationRecord
   belongs_to :agreement, class_name: "LabourMarket::Agreement", foreign_key: "agreement_id", optional: true
-  belongs_to :period, class_name: "LabourMarket::Period", foreign_key: "period_id", optional: true
   has_many :references, as: :referenceable, inverse_of: :referenceable
   has_one_attached :file
 
   enum document_language: [:sv, :en]
 
   scope :agreement_missing, -> { where(agreement_id: nil) }
-  scope :period_missing, -> { where(period_id: nil) }
   scope :reference_missing, -> {
     left_outer_joins(:references).where(labour_market_references: { id: nil })
   }
@@ -20,7 +18,6 @@ class LabourMarket::Document < ApplicationRecord
       scopes [
         nil,
         :agreement_missing,
-        :period_missing,
         :reference_missing,
       ]
       field :file
