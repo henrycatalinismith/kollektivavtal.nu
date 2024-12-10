f664246dc577fad324c8ef90047d128c74584e34
Henry Catalini Smith <henry@catalinismith.se>
2024-12-10 21:38:58 +0100

    Rework admin workflow

--- app/models/labour_market/agreement.rb
+++ app/models/labour_market/agreement.rb
@@ -13,17 +13,21 @@ class LabourMarket::Agreement < ApplicationRecord
     :agreement_public,
   ]
 
-  scope :description_missing, -> {
-    where(agreement_description: [nil, ""])
-  }
+  scope :description_missing, -> { where(agreement_description: [nil, ""]) }
+  scope :description_present, -> { where.not(agreement_description: [nil, ""]) }
 
-  scope :scope_missing, -> {
-    where(agreement_scope: [nil, ""])
-  }
+  scope :scope_missing, -> { where(agreement_scope: [nil, ""]) }
+  scope :scope_present, -> { where.not(agreement_scope: [nil, ""]) }
 
   scope :documents_missing, -> {
     left_outer_joins(:documents)
       .where(labour_market_documents: { id: nil })
+      .distinct
+  }
+
+  scope :documents_present, -> {
+    left_outer_joins(:documents)
+      .where.not(labour_market_documents: { id: nil })
       .distinct
   }
 
@@ -33,14 +37,52 @@ class LabourMarket::Agreement < ApplicationRecord
       .distinct
   }
 
+  scope :organisations_present, -> {
+    left_outer_joins(:signatures)
+      .where.not(labour_market_signatures: { id: nil })
+      .distinct
+  }
+
   scope :references_missing, -> {
     left_outer_joins(:references)
       .where(labour_market_references: { id: nil })
   }
 
+  scope :references_present, -> {
+    left_outer_joins(:references)
+      .where.not(labour_market_references: { id: nil })
+      .distinct
+  }
+
   scope :translations_missing, -> {
     left_outer_joins(:translations)
       .where(labour_market_translations: { id: nil })
+  }
+
+  scope :translations_present, -> {
+    left_outer_joins(:translations)
+      .where.not(labour_market_translations: { id: nil })
+      .distinct
+  }
+
+  scope :agreement_complete, -> {
+    description_present
+      .scope_present
+      .documents_present
+      .organisations_present
+      .references_present
+      .translations_present
+  }
+
+  scope :agreement_incomplete, -> {
+    base_query = left_outer_joins(:documents, :signatures, :references, :translations)
+    base_query.where(agreement_description: [nil, ""])
+      .or(base_query.where(agreement_scope: [nil, ""]))
+      .or(base_query.where(labour_market_documents: { id: nil }))
+      .or(base_query.where(labour_market_signatures: { id: nil }))
+      .or(base_query.where(labour_market_references: { id: nil }))
+      .or(base_query.where(labour_market_translations: { id: nil }))
+      .distinct
   }
 
   before_create :set_slug
@@ -57,6 +99,21 @@ class LabourMarket::Agreement < ApplicationRecord
     end
   end
 
+  def agreement_complete?
+    props = [
+      :agreement_name,
+      :agreement_description,
+      :agreement_scope,
+    ]
+    return false if props.any? do |attribute|
+      self.send(attribute).blank?
+    end
+    return false if self.documents.empty?
+    return false if self.members.empty?
+    return false if self.translations.empty?
+    return true
+  end
+
   rails_admin do
     object_label_method do
       :agreement_name
@@ -65,15 +122,15 @@ class LabourMarket::Agreement < ApplicationRecord
     list do
       scopes [
         nil,
-        :description_missing,
-        :scope_missing,
-        :documents_missing,
-        :organisations_missing,
-        :references_missing,
-        :translations_missing,
+        :agreement_complete,
+        :agreement_incomplete,
         :agreement_hidden,
         :agreement_public,
       ]
+      field :agreement_complete?, :boolean do
+        label "Ready?"
+      end
+
       field :agreement_name
       field :agreement_slug
       field :created_at
@@ -100,6 +157,43 @@ class LabourMarket::Agreement < ApplicationRecord
       field :references
       field :translations
     end
+
+    show do
+      field :agreement_name
+      field :agreement_slug
+
+      field :agreement_complete?, :boolean
+
+      field :missing_fields do
+        label "Missing Fields"
+        pretty_value do
+          bindings[:view].content_tag(:ul) {
+            value.map do |field|
+              bindings[:view].content_tag(:li, field)
+            end.join.html_safe
+          }.html_safe
+        end
+      end
+
+      field :agreement_description
+      field :agreement_scope
+      field :agreement_visibility
+      field :members
+      field :references
+      field :translations
+    end
+  end
+
+  def missing_fields
+    fields = []
+    fields << "Name" if self.agreement_name.blank?
+    fields << "Description" if self.agreement_description.blank?
+    fields << "Scope" if self.agreement_scope.blank?
+    fields << "Documents" if self.documents.empty?
+    fields << "Organisations" if self.members.empty?
+    fields << "Translations" if self.translations.empty?
+    fields << "References" if self.references.empty?
+    fields
   end
 
   def to_param
