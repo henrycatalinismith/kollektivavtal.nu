daac39306edd3e6457bcc88cb76a6dd7290dc3ef
Henry Catalini Smith <henry@catalinismith.se>
2024-12-16 19:18:14 +0100

    document names:

---
+++ app/helpers/labour_market/documents_helper.rb
@@ -0,0 +1,14 @@
+module LabourMarket::DocumentsHelper
+  def document_name(document)
+    if I18n.locale == :sv
+      return document.agreement_name
+    end
+    translation = document.translations.find {
+      |t| t.document_name? and t.translation_language == I18n.locale.to_s
+    }
+    if translation.present?
+      return translation.translation_text
+    end
+    return document.document_name
+  end
+end
