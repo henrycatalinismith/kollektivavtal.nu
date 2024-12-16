48027a01beb28c9ec3371aa9d4d1ad6a167e3c5d
Henry Catalini Smith <henry@catalinismith.se>
2024-12-16 20:23:28 +0100

    Fix bug

--- app/helpers/labour_market/documents_helper.rb
+++ app/helpers/labour_market/documents_helper.rb
@@ -1,7 +1,7 @@
 module LabourMarket::DocumentsHelper
   def document_name(document)
     if I18n.locale == :sv
-      return document.agreement_name
+      return document.document_name
     end
     translation = document.translations.find {
       |t| t.document_name? and t.translation_language == I18n.locale.to_s
